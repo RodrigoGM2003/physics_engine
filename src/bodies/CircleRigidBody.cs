ee3742d9c0c60f8451c367976d6ea551aad89882
rodrigogm <rodrigogm@correo.ugr.es>
2024-08-01 16:47:07 +0200

    Added rigidbody templates

---
+++ src/bodies/CircleRigidBody.cs
@@ -0,0 +1,30 @@
+using System;
+using SFML.Graphics;
+using SFML.Window;
+using SFML.System;
+using System.Data;
+
+namespace PhysicsEngine
+{
+    /**
+     * Class for all physic bodies in the simulation
+     */
+    public class CircleRigidBody : RigidBody
+    {
+        public float Radius { get; protected set; }
+
+        /**
+        * Constructor for the CircleRigidBody class
+        * @param mass The mass of the object in kg
+        * @param collider The collider for the object
+        * @param drawer The drawer for the object
+        */
+        public CircleRigidBody(float radius, RenderWindow window, Color? color = null, float? elasticity = null, float? friction = null,
+                                float? mass = null, Vector2f? velocity = null, Vector2f? acceleration = null)
+        : base(new CircleCollider(new Vector2f(0, 0), radius, elasticity, friction), new CircleDrawer(window, radius, color), 
+           mass: mass, velocity: velocity, acceleration: acceleration)
+        {
+            Radius = radius;
+        }
+    }
+}
---
+++ src/bodies/RectangleRigidbody.cs
@@ -0,0 +1,30 @@
+using System;
+using SFML.Graphics;
+using SFML.Window;
+using SFML.System;
+using System.Data;
+
+namespace PhysicsEngine
+{
+    /**
+     * Class for all physic bodies in the simulation
+     */
+    public class RectangleRigidBody : RigidBody
+    {
+        public Vector2f Size { get; protected set; }
+
+        /**
+        * Constructor for the RectangleRigidBody class
+        * @param mass The mass of the object in kg
+        * @param collider The collider for the object
+        * @param drawer The drawer for the object
+        */
+        public RectangleRigidBody(Vector2f size, RenderWindow window, Color? color = null, float? elasticity = null, float? friction = null,
+                                float? mass = null, Vector2f? velocity = null, Vector2f? acceleration = null)
+        : base(new RectangleCollider(new Vector2f(0, 0), size, elasticity, friction), new RectangleDrawer(window, size, color), 
+           mass: mass, velocity: velocity, acceleration: acceleration)
+        {
+            Size = size;
+        }
+    }
+}
